81bedf1aaf90505099b8d3e75cc99df6fb599fe4
Tommi Puurunen <Tommi.Puurunen@edu.savonia.fi>
2020-05-01 17:45:59 +0300

    roosan muutokset ja muutama käyttöliittymän kaunnistus

--- MOVA2020/forms/mokkimuokkaus.cs
+++ MOVA2020/forms/mokkimuokkaus.cs
@@ -14,7 +14,6 @@ namespace MOVA2020.forms
 {
     public partial class Mokkimuokkaus : Form
     {
-        //fix
         Primary p;
         Mokki m;
 
@@ -43,20 +42,53 @@ namespace MOVA2020.forms
             this.rtbKuvaus.Text = m.Kuvaus;
             this.btnMokinLisays.Text = "Tallenna Muokkaus";
         }
-
-        private void btnLisays_Click(object sender, EventArgs e)
-        {
-            //ei tämä
-        }
-
         private void btnMokinLisays_Click(object sender, EventArgs e)
         {
             //jos kaikki kentät eivät ole täytettynä, se ei anna lisätä mökkiä tietokantaan
-            if (cbToimialue.Text.Length == 0 || tbMokkiNimi.TextLength == 0 || tbPostinumero.TextLength == 0
-                || tbKatuosoite.TextLength == 0 || tbMokkiHinta.TextLength == 0 || rtbVarustelu.Text.Length == 0
+            if (cbToimialue.SelectedIndex == -1 || tbMokkiNimi.Text.Length == 0 || tbPostinumero.Text.Length == 0
+                || tbKatuosoite.Text.Length == 0 || tbMokkiHinta.Text.Length == 0 || rtbVarustelu.Text.Length == 0
                 || rtbKuvaus.Text.Length == 0)
             {
-                MessageBox.Show("Kaikki kentät tulee olla täytettynä, ennen kuin voit lisätä mökin.", "Virhe", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (cbToimialue.SelectedIndex == -1)
+                {
+                    errorProvider1.SetError(cbToimialue, "Toimialue puuttuu");
+                }
+                else
+                {
+                    errorProvider1.SetError(cbToimialue, "");
+                }
+                if (tbMokkiNimi.TextLength == 0)
+                {
+                    errorProvider1.SetError(tbMokkiNimi, "Mökin nimi puuttuu");
+                }
+                else
+                {
+                    errorProvider1.SetError(tbMokkiNimi, "");
+                }
+                if (tbPostinumero.TextLength < 5 || tbPostinumero.TextLength > 5)
+                {
+                    errorProvider1.SetError(tbPostinumero, "Postinumero puuttuu");
+                }
+                else
+                {
+                    errorProvider1.SetError(tbPostinumero, "");
+                }
+                if (tbKatuosoite.TextLength == 0)
+                {
+                    errorProvider1.SetError(tbKatuosoite, "Katuosoite puuttuu");
+                }
+                else
+                {
+                    errorProvider1.SetError(tbKatuosoite, "");
+                }
+                if (tbMokkiHinta.TextLength == 0)
+                {
+                    errorProvider1.SetError(tbMokkiHinta, "Hinta puuttuu");
+                }
+                else
+                {
+                    errorProvider1.SetError(tbMokkiHinta, "");
+                }
             }
             else
             {
