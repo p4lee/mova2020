bbf847c70ead232bd4f57b7c1d3957aee8b50406
Jonna Räsänen <Jonna.Rasanen@edu.savonia.fi>
2020-05-06 13:16:54 +0300

    Toimiva testisähköposti

--- MOVA2020/forms/Laskutus.cs
+++ MOVA2020/forms/Laskutus.cs
@@ -72,28 +72,9 @@ namespace MOVA2020.forms
         }
         private void btlaheta_Click(object sender, EventArgs e)
         {
-            //Nappi, jota painamalla lasku lähetetään asiakkaan sähköpostiin
-            try
-            {
-                MailMessage mail = new MailMessage();
-                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-
-                mail.From = new MailAddress("your_email_address@gmail.com");
-                mail.To.Add("to_address");
-                mail.Subject = "Test Mail";
-                mail.Body = "This is for testing SMTP mail from GMAIL";
-
-                SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential("username", "password");
-                SmtpServer.EnableSsl = true;
-
-                SmtpServer.Send(mail);
-                MessageBox.Show("mail Send");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
+            //Nappi, joka aukaisee lomakkeen, jolla kysytään lähetyssähköpostia
+            sähkoposti sp = new sähkoposti();
+            sp.Show();
         }
 
         private void btnVaraustiedot_Click(object sender, EventArgs e)
